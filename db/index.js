521a3abc6f68b98257b0f19c282ddbe03d966d0a
Jacob <your_email@real_email.edu>
2022-03-24 09:56:52 -0500

    broke the database

---
+++ db/index.js
@@ -0,0 +1,40 @@
+const { Client } = require('pg'); // imports the pg module
+
+// supply the db name and location of the database
+const client = new Client('postgres://localhost:5432/juicebox-dev')
+
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+  
+  // and export them
+ 
+
+
+
+async function createUser({ username, password }) {
+    try {
+      const result = await client.query(`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2);
+      `, [username, password]);
+  
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  // later
+  module.exports = {
+    client,
+    getAllUsers,
+    createUser,
+  }
+
