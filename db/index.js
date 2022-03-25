543105e3a22403447439b5ac0f8a3e9efaaded22
Jacob <your_email@real_email.edu>
2022-03-25 13:17:13 -0500

    refactoring old functions

--- db/index.js
+++ db/index.js
@@ -133,12 +133,16 @@ async function updatePost(id, fields = {}) {
 
 async function getAllPosts() {
   try {
-    const { rows } = await client.query(`
-      SELECT *
+    const { rows: postIds } = await client.query(`
+      SELECT id
       FROM posts;
     `);
 
-    return rows;
+    const posts = await Promise.all(postIds.map(
+      post => getPostById( post.id )
+    ));
+
+    return posts;
   } catch (error) {
     throw error;
   }
@@ -146,13 +150,17 @@ async function getAllPosts() {
 
 async function getPostsByUser(userId) {
   try {
-    const { rows } = await client.query(`
-      SELECT * 
-      FROM posts
+    const { rows: postIds } = await client.query(`
+      SELECT id 
+      FROM posts 
       WHERE "authorId"=${ userId };
     `);
 
-    return rows;
+    const posts = await Promise.all(postIds.map(
+      post => getPostById( post.id )
+    ));
+
+    return posts;
   } catch (error) {
     throw error;
   }
@@ -264,7 +272,6 @@ module.exports = {
   createPost,
   updatePost,
   getAllPosts,
-  getPostsByUser,
   createTags,
   addTagsToPost
 }
