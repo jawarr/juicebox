e82353539ee03aba05031a068f7a49aa49093d86
Jacob <your_email@real_email.edu>
2022-03-28 10:36:48 -0500

    secrets

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { client } = require('./db');
 client.connect();
 
@@ -21,6 +23,36 @@ server.use((req, res, next) => {
   
     next();
   });
+
+// server.use(async (req, res, next) => {
+//     const prefix = 'Bearer '
+//     const auth = req.headers['Authorization'];
+
+//     if (!auth) {
+//         next(); // don't set req.user, no token was passed in
+//     }
+
+
+//     if (auth.startsWith(prefix)) {
+//         // recover the token
+//         const token = auth.slice(prefix.length);
+//         try {
+//         // recover the data
+//         const { id } = jwt.verify(data, 'secret message');
+
+//         // get the user from the database
+//         const user = await getUserById(id);
+//         // note: this might be a user or it might be null depending on if it exists
+
+//         // attach the user and move on
+//         req.user = user;
+
+//         next();
+//         } catch (error) {
+//         // there are a few types of errors here
+//         }
+//     }
+// })
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
