35a45d50a677b7f939f1f6d8da560bac9aba103b
Jacob <your_email@real_email.edu>
2022-03-28 12:55:26 -0500

    Merge branch 'main' of github.com:jawarr/juicebox into main

--- db/index.js
+++ db/index.js
@@ -83,6 +83,20 @@ async function getUserById(userId) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 /**
  * POST Methods
  */
@@ -336,5 +350,6 @@ module.exports = {
   createTags,
   addTagsToPost,
   getPostsByTagName,
-  getAllTags
+  getAllTags,
+  getUserByUsername
 }
