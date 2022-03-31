d2c3a29a175100910954229de697bd9edb793419
Jacob <your_email@real_email.edu>
2022-03-31 09:28:28 -0500

    delete posts api

--- api/posts.js
+++ api/posts.js
@@ -7,7 +7,29 @@ const { updatePost  } = require('../db');
 const { getPostById } = require('../db');
 const { requireUser } = require('./utils');
 
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
 
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
 postsRouter.post('/', requireUser, async (req, res, next) => {
   const { title, content, tags = "" } = req.body;
 
